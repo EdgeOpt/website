b58f9b032d0a6cd119b2e215352d24388ed7677c
Selvakumar Ulaganathan <ulag.selvam@gmail.com>
2020-10-06 00:48:54 +0200

    Course registration option is being tested

--- contact_process.php
+++ contact_process.php
@@ -1,12 +1,16 @@
 <?php 
 
-// The message
-$message = "Line 1\r\nLine 2\r\nLine 3";
 
-// In case any of our lines are larger than 70 characters, we should use wordwrap()
-$message = wordwrap($message, 70, "\r\n");
-
-// Send
-mail('edgeopt@gmail.com', 'My Subject', $message);
+$to = 'edgeopt@gmail.com';
+$subject = 'dsdasa';
+$message = 'vvv'; 
+$from = 'edgeai.prospect@gmail.com';
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 
 ?>
